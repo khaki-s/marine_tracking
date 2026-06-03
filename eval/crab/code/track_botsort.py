2fae34d51b4c2b521ab183b0faa921e705043fb0
khaki-s <18353398098@163.com>
2026-06-03 16:54:45 +0800

    modified:   eval/crab/code/track_botsort.py

--- eval/crab/code/track_botsort.py
+++ eval/crab/code/track_botsort.py
@@ -1,7 +1,5 @@
-import cv2
 import numpy as np
 from ultralytics import YOLO
-import os
 
 # ------------------ TrackEval Output Settings ------------------
 TRACKEVAL_OUTPUT_DIR = './result19-4-12'  # 对应 template2.yaml 的 trackers_folder
